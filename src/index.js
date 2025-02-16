73c49acb0e6f827d4f752f59dd12cf19e72da1c8
mirellaoliveira244 <165833881+mirellaoliveira244@users.noreply.github.com>
2025-02-16 00:12:15 +0000

    api

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 dotenv.config();
 
 const app = express(); // Instancia o Express
 const port = 3000; // Define a porta
 
-app.use(roteadorUsuario);
 app.use(express.json());
+app.use(roteadorUsuario);
+app.use(roteadorLogin);
 
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
